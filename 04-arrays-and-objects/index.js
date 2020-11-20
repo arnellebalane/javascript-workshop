1051a868d7c8f91fecb3e7693a7cd1cf97c3035e
Arnelle Balane <arnellebalane@gmail.com>
2020-11-21 06:10:17 +0800

    Add exercise for arrays and objects

---
+++ 04-arrays-and-objects/index.js
@@ -0,0 +1,23 @@
+/**
+ * 1. Declare a function named "isNumberInArrayLoop".
+ *    This function should accept two arguments, "numbers" and "num".
+ *    Using a loop, return true or false whether "num" is in the "numbers" array.
+ */
+
+/**
+ * 2. Declare a function named "isNumberInArray".
+ *    This function should accept two arguments, "numbers" and "num".
+ *    Using a built-in array method, return true or false whether "num" is in
+ *      the "numbers" array.
+ */
+
+/**
+ * 3. Declare a variable named "me".
+ *    Set its value to an object with the following properties:
+ *      - firstName : Your first name
+ *      - lastName : Your last name
+ *      - favoriteNumbers : An array of your favorite numbers
+ *      - socialAccounts : An object with the following properties:
+ *        - facebook : Link to your Facebook profile
+ *        - twitter : Link to your Twitter profile
+ */
---
+++ 04-arrays-and-objects/test.js
@@ -0,0 +1,28 @@
+const assert = chai.assert;
+
+describe('Arrays and Objects', () => {
+  it('Declare a function named "isNumberInArrayLoop".', () => {
+    assert.typeOf(isNumberInArrayLoop, 'function');
+    assert.isTrue(isNumberInArrayLoop([1, 2, 3], 3));
+    assert.isTrue(isNumberInArrayLoop([1, 2, 3], 4));
+  });
+
+  it('Declare a function named "isNumberInArray".', () => {
+    assert.typeOf(isNumberInArray, 'function');
+    assert.isTrue(isNumberInArray([1, 2, 3], 3));
+    assert.isTrue(isNumberInArray([1, 2, 3], 4));
+  });
+
+  it('Declare a variable named "me".', () => {
+    assert.typeOf(me, 'object');
+    assert.typeOf(me.firstName, 'string');
+    assert.typeOf(me.lastName, 'string');
+    assert.isTrue(me.favoriteNumbers instanceof Array);
+    me.favoriteNumbers.forEach((number) => {
+      assert.typeOf(number, 'number');
+    });
+    assert.typeOf(me.socialAccounts, 'object');
+    assert.typeOf(me.socialAccounts.facebook, 'string');
+    assert.typeOf(me.socialAccounts.twitter, 'string');
+  });
+});
