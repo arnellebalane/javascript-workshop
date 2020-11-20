e822334312dda01ef990bf5ea8b07efdb24ce3ce
Arnelle Balane <arnellebalane@gmail.com>
2020-11-21 05:39:38 +0800

    Add exercise for conditional and iteration

---
+++ 02-conditionals-and-iteration/index.js
@@ -0,0 +1,15 @@
+/**
+ * 1. Using a loop, get the sum of the first 10 positive integers.
+ *    Store this result in a variable named "answer1".
+ */
+
+/**
+ * 2. Using a loop, get the sum of the first 10 positive integers that are
+ *      divisible by 3 or 5.
+ *    Store this result in a variable named "answer2".
+ */
+
+/**
+ * 3. Using a loop, get the product of the first 13 positive integers.
+ *    Store whether this number is an even number in a variable named "answer3".
+ */
---
+++ 02-conditionals-and-iteration/test.js
@@ -0,0 +1,15 @@
+const assert = chai.assert;
+
+describe('Conditionals and Iteration', () => {
+  it('Using a loop, get the sum of the first 10 positive integers. Store this result in a variable named "answer1".', () => {
+    assert.equal(answer1, 55);
+  });
+
+  it('Using a loop, get the sum of the first 10 positive integers that are divisible by 3 or 5. Store this result in a variable named "answer2".', () => {
+    assert.equal(answer2, 119);
+  });
+
+  it('Using a loop, get the product of the first 13 positive integers. Store whether this number is an even number in a variable named "answer3".', () => {
+    assert.isTrue(answer3);
+  });
+});
